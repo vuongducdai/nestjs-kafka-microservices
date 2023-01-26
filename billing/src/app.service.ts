ff5f6ac2d8c2bc62e4c40261647f589d989a6917
Vuong Duc Dai <>
2023-01-26 10:15:29 +0700

    Setup Kafka Microservice for billing service

--- billing/src/app.service.ts
+++ billing/src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { OrderCreatedEvent } from './order-created.event';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  handleOrderCreated(orderCreatedEvent: OrderCreatedEvent) {
+    const { userId, price } = orderCreatedEvent;
+    console.log(userId, price);
   }
 }
