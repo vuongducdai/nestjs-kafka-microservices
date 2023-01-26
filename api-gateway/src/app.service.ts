496f181acb693b91565742a2b304339d08c1aae5
Vuong Duc Dai <>
2023-01-26 09:57:50 +0700

    Setup event emit for API gateway

--- api-gateway/src/app.service.ts
+++ api-gateway/src/app.service.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientKafka } from '@nestjs/microservices';
 import { CreateOrderDto } from './create-order.dto';
+import { CreateOrderEvent } from './order-created.event';
 
 @Injectable()
 export class AppService {
@@ -15,5 +16,13 @@ export class AppService {
 
   createOrder(createOrderDto: CreateOrderDto) {
     const { userId, price } = createOrderDto;
+
+    // The second parameter will automatically stringify if it's object
+    // In this case we pass in a class, Nestjs will call toString in that class
+    // So we need to define our own toString method
+    this.billingClient.emit(
+      'order_created',
+      new CreateOrderEvent('123', userId, price),
+    );
   }
 }
