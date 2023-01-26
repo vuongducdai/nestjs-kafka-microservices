5f3fd6e031b9926de94dbbd0e3e6b8ac4ff66a23
Vuong Duc Dai <>
2023-01-26 09:44:22 +0700

    Setup CreateOrderDTO

--- api-gateway/src/app.service.ts
+++ api-gateway/src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { CreateOrderDto } from './create-order.dto';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  createOrder(createOrderDto: CreateOrderDto) {
+    const { userId, price } = createOrderDto;
   }
 }
