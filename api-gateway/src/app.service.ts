7e82bce5ae62e432f53f0e8625dff4b030b6f4ac
Vuong Duc Dai <>
2023-01-26 09:50:05 +0700

    Setup Kafka

--- api-gateway/src/app.service.ts
+++ api-gateway/src/app.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
 import { CreateOrderDto } from './create-order.dto';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject('BILLING_SERVICE')
+    private readonly billingClient: ClientKafka,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
   }
